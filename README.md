0b5089d8f03a099f776a7469262c82e60c02a039
Willin Wang <willin@willin.org>
2018-04-13 10:24:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# @dwing/port
+
+[![github](https://img.shields.io/github/followers/willin.svg?style=social&label=Follow)](https://github.com/willin) [![npm](https://img.shields.io/npm/v/@dwing/port.svg)](https://npmjs.org/package/@dwing/port) [![npm](https://img.shields.io/npm/dt/@dwing/port.svg)](https://npmjs.org/package/@dwing/port)
+
+## Useage
+
+```js
+const port = require('@dwing/port');
+
+port(1234).then(result => {
+  // true: available
+  // false: in use
+});
+```
+
+## License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+module.exports = p => new Promise((resolve) => {
+  const port = parseInt(p, 10);
+  // eslint-disable-next-line no-restricted-globals
+  if (isNaN(port) || port < 0 || port > 65536) {
+    resolve(false);
+  }
+
+  const tester = net.createServer()
+    // catch errors, and resolve false
+    .once('error', () => {
+      resolve(false);
+    })
+    // return true if success
+    .once('listening', () => tester.once('close', () => resolve(true)).close())
+    .listen(port);
+});
